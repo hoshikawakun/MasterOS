97495434232e6015ecf333f605f90db8e9d0d4b4
hoshikawakun <puffy.a@hotmail.com>
2021-02-27 21:50:06 -0300

    Update adicionarServico.php

--- application/views/servicos/adicionarServico.php
+++ application/views/servicos/adicionarServico.php
@@ -28,13 +28,9 @@
                             <input id="descricao" type="text" name="descricao" value="<?php echo set_value('descricao'); ?>" />
                         </div>
                     </div>
-                    <div class="form-actions">
-                        <div class="span12">
-                            <div class="span6 offset3">
-                                <button type="submit" class="btn btn-success"><i class="fas fa-plus"></i> Adicionar</button>
-                                <a href="<?php echo base_url() ?>index.php/servicos" id="btnAdicionar" class="btn"><i class="fas fa-backward"></i> Voltar</a>
-                            </div>
-                        </div>
+                    <div class="form_actions" align="center">
+                    <button type="submit" class="btn btn-success"><i class="fas fa-plus"></i> Adicionar</button>
+                    <a href="<?php echo base_url() ?>index.php/produtos" id="" class="btn btn-warning"><i class="fas fa-backward"></i> Voltar</a>
                     </div>
                 </form>
             </div>
