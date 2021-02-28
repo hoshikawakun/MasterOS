f5e7905621a5ccd2032248d1130c28888e32cf90
hoshikawakun <puffy.a@hotmail.com>
2021-02-28 01:42:02 -0300

    Update imprimirVenda.php

--- application/views/vendas/vendas.php
+++ application/views/vendas/vendas.php
@@ -3,14 +3,15 @@
 <?php } ?>
 
 <div class="widget-box">
-    <div class="widget-title">
-        <span class="icon">
-            <i class="fas fa-cash-register"></i>
-        </span>
-        <h5>Vendas</h5>
+	<div class="widget-title">
+    <span class="icon"><i class="fas fa-cash-register"></i></span>
+    <h5>Vendas</h5>
     </div>
-    <div class="widget-content nopadding tab-content">
-        <table id="tabela" class="table">
+    <div class="widget-content nopadding">
+    <!--
+    <div class="widget_box_Painel2">
+    -->
+        <table id="tabela" width="100%" class="table_p">
             <thead>
                 <tr style="background-color: #2D335B">
                     <th>#</th>
@@ -62,18 +63,18 @@
 <?php echo $this->pagination->create_links(); ?>
 
 <!-- Modal -->
-<div id="modal-excluir" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="modal-excluir" class="modal hide fade widget_box_vizualizar4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <form action="<?php echo base_url() ?>index.php/vendas/excluir" method="post">
-        <div class="modal-header">
+        <div class="modal_header_anexos">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h5 id="myModalLabel">Excluir Venda</h5>
+            <h3 id="myModalLabel">Excluir Venda</h3>
         </div>
         <div class="modal-body">
             <input type="hidden" id="idVenda" name="id" value="" />
             <h5 style="text-align: center">Deseja realmente excluir esta Venda?</h5>
         </div>
         <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
+            <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true">Cancelar</button>
             <button class="btn btn-danger">Excluir</button>
         </div>
     </form>
