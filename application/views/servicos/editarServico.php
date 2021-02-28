defe204ac45a19d2219fc7297a249736d9578e9d
hoshikawakun <puffy.a@hotmail.com>
2021-02-27 21:50:13 -0300

    Update editarServico.php

--- application/views/servicos/editarServico.php
+++ application/views/servicos/editarServico.php
@@ -29,13 +29,9 @@
                             <input id="descricao" type="text" name="descricao" value="<?php echo $result->descricao ?>" />
                         </div>
                     </div>
-                    <div class="form-actions">
-                        <div class="span12">
-                            <div class="span6 offset3">
-                                <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i> Atualizar</button>
-                                <a href="<?php echo base_url() ?>index.php/servicos" id="btnAdicionar" class="btn"><i class="fas fa-backward"></i> Voltar</a>
-                            </div>
-                        </div>
+                    <div class="form_actions" align="center">
+                    <button type="submit" class="btn btn-primary">Salvar Alterações</button>
+                    <a href="<?php echo base_url() ?>index.php/servicos" id="btnAdicionar" class="btn btn-warning"><i class="fas fa-backward"></i> Voltar</a>
                     </div>
                 </form>
             </div>
