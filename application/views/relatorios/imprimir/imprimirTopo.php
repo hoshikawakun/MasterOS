6dad5f2dbcde8f7b43c389251f9583d9b2395652
hoshikawakun <puffy.a@hotmail.com>
2021-03-01 21:32:42 -0300

    Update imprimirTopo.php

--- application/views/relatorios/imprimir/imprimirTopo.php
+++ application/views/relatorios/imprimir/imprimirTopo.php
@@ -1,7 +1,7 @@
 <div>
     <br>
-    <div style="width: 30%; float: left" class="float-left col-md-3">
-        <img style="width: 200px" src="<?= $emitente[0]->url_logo ?>" alt=""><br><br>
+    <div style="width: 30%; float: left" class="float-left col-md-3"><br>
+        <img style="width: 200px" src="<?= $emitente[0]->url_logo ?>" alt="LOGO">
     </div>
     <div style="float: right; font-size: 10px"><span style="font-size: 10px"><b>Empresa: </b><?= $emitente[0]->nome ?></span><br>
     <span style="font-size: 10px"><b>CNPJ: </b><?= $emitente[0]->cnpj ?></span><br>
