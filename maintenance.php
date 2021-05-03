e6404fc90d61d5b454352222ff8cc85f963f066e
hoshikawakun <puffy.a@hotmail.com>
2021-05-02 21:09:07 -0300

    Pag em Manutenção

---
+++ maintenance.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Em Manutenção</title>
+</head>
+<style type="text/css">
+body {
+        overflow: hidden;
+    }
+div {
+	position: relative;
+    height: 100vh;
+    width: 100vw;
+	background-image: url(assets/img/142357.jpg);
+	background-size: cover;
+	overflow-x: hidden;
+}
+</style>
+<table>
+  <tr>
+    <td><div align="center"><img src="assets/img/manutenção.png" width="800""></div></td>
+  </tr>
+</table>
