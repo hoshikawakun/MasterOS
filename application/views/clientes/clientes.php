e857543a4a3fd92cb745978aed898a8395915b32
hoshikawakun <puffy.a@hotmail.com>
2021-08-09 11:28:12 -0300

    Update clientes.php

--- application/views/clientes/clientes.php
+++ application/views/clientes/clientes.php
@@ -33,7 +33,8 @@
 						
                         echo '<tr>';
                         echo '<td><div align="center">' . $r->idClientes . '</div></td>';
-						echo '<td>' . $NomeClienteShort . '</td>';
+						/*echo '<td>' . $NomeClienteShort . '</td>';*/
+						echo '<td><a href="' . base_url() . 'index.php/mine?e=' . $r->email . '&c=' . $r->senha . '" target="new" style="margin-right: 1%">' . $NomeClienteShort . '</a></td>';
                         echo '<td><div align="center">' . $r->documento . '</div></td>';
                         echo '<td><div align="center">' . $r->senha . '</div></td>';
                         echo '<td><div align="center">' . $r->telefone . '</div></td>';
@@ -41,7 +42,7 @@
                         echo '<td><div align="center">';
                         if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vCliente')) {
                             echo '<a href="' . base_url() . 'index.php/clientes/visualizar/' . $r->idClientes . '" style="margin-right: 1%" class="btn tip-top" title="Visualizar mais detalhes"><i class="fas fa-eye"></i></a>';
-							echo '<a href="' . base_url() . 'index.php/mine?e=' . $r->email . '&c=' . $r->senha . '" target="new" style="margin-right: 1%" class="btn btn-warning tip-top" title="Área do cliente"><i class="fas fa-key"></i></a>';
+							/*echo '<a href="' . base_url() . 'index.php/mine?e=' . $r->email . '&c=' . $r->senha . '" target="new" style="margin-right: 1%" class="btn btn-warning tip-top" title="Área do cliente"><i class="fas fa-key"></i></a>';*/
                         }
 						if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vCliente')) {
                                 $zapnumber = preg_replace("/[^0-9]/", "", $r->telefone);
